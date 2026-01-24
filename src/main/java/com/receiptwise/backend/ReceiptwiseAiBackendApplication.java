3eccedd38ce2762c8b0f562c2c785968b9a4c7d0
Ioannis <glaventzakis@gmail.com>
2026-01-24 01:45:31 +0100

    first commit

---
+++ src/main/java/com/receiptwise/backend/ReceiptwiseAiBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.receiptwiseaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptwiseAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptwiseAiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/receiptwise/backend/ReceiptwiseAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.receiptwiseaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptwiseAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
